7054e6ddc5d60ba6cec466e6bf5b92d00e95b875
Prasad Sawant <58025997+prasadsawant7@users.noreply.github.com>
2023-06-24 02:34:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
 
+app = FastAPI()
+
+class TextData(BaseModel):
+    text: str
+
+with open("keybert.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.post("/")
+async def get_keywords_from_text(text_data: TextData):
+    keywords = model.extract_keywords(text_data.text, top_n=10, stop_words="english")
+    filtered_keywords = [
+        [keyword, distance] for keyword, distance in keywords if distance >= 0
+    ]
+    return dict(filtered_keywords)
